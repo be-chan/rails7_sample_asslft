c25fc077fa8179e432d7186e8c4e4bb9a238b7f6
be-chan <70454487+be-chan@users.noreply.github.com>
2024-02-04 15:24:46 +0900

    docker環境のエラーが出ていたので、修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+  # dockerやvargrant使用している場合は必要
+  BetterErrors::Middleware.allow_ip! "0.0.0.0/0"
+  config.web_console.whitelisted_ips = '192.168.128.1'
 end
